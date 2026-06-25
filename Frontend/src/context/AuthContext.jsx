afe051fd89aa078437850bad8d88f04bea2c0d65
MD Shishir <shishir.jstu@gmail.com>
2026-06-25 21:04:08 +0600

    Update AuthContext.jsx

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ import { createContext } from 'react';
 export const authDataContext = createContext();
 
 const AuthContext = ({ children }) => {
-    let serverUrl = 'http://localhost:8000';
+    let serverUrl = 'https://mern-e-commerce-1-ghee.onrender.com';
 
     let value = {
         serverUrl
