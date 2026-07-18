f0aaefbf05a07f52b3fe83247d374cb065f5e006
shishir-stack <shishir.jstu@gmail.com>
2026-07-18 12:28:54 +0600

    louout update

--- Backend/controller/authcontroller.js
+++ Backend/controller/authcontroller.js
@@ -64,13 +64,19 @@ export const login = async (req,res) => {
     }
 }
 
-export const logout =async(req,res)=>{
+export const logout = async (req, res) => {
     try {
-        res.clearCookie("token")
-             return res.status(200).json({message:'LogOut Success'});
+        
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,      
+            sameSite: "none",  
+        });
+        
+        return res.status(200).json({ message: 'LogOut Success' });
     } catch (error) {
          console.log("Logout Error");
-        return res.status(500).json({message:`Logout Error ${error}`});
+        return res.status(500).json({ message: `Logout Error ${error}` });
     }
 }
 
